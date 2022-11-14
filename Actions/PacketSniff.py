eb9cd108bd4f969b7eb924911be8991ecc953092
da3rny <barisakyildiz3@gmail.com>
2022-11-14 12:57:00 +0300

    a few bug fixing on tcp parsing

--- Actions/PacketSniff.py
+++ Actions/PacketSniff.py
@@ -5,22 +5,41 @@ from textwrap import wrap
 
 class PacketSniffer:
     def __init__(self):
-        pass
+        self.TAB_1 = "\t - "
+        self.TAB_2 = "\t\t - "
+        self.TAB_3 = "\t\t\t - "
+        self.TAB_4 = "\t\t\t\t - "
 
-    def getIp(address):
+    def getIp(self, address):
         return '.'.join(map(str, address))
     
     def initSocket(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
         while True:
-            raw_data, addr = s.recvfrom(65535) #65565
+            raw_data, addr = s.recvfrom(14) #65565
             dest, src, proto, data = self.ethernetHead(raw_data=raw_data)
-            print("--- Ethernet Frame Results --- \nDestination {}, Source: {}\nData: {}\nProtocol: {}\n\n\n".format(dest, src, data, proto))
+            print("--- Ethernet Frame Results --- \nDestination {}, Source: {}\nProtocol: {}\n\n\n".format(dest, src, proto))
             if proto == 8:
                 ipv4_head = self.ipv4Head(data)
-                print('\t - ' + 'IPv4 Packet Results:')
-                print('\t\t - ' + 'Version: {}, Header Length: {}, TTL: {}'.format(ipv4_head[0], ipv4_head[1], ipv4_head[2]))
-                print('\t\t - ' + 'Protocol: {}, Source: {}, Target: {}'.format(ipv4_head[3], ipv4_head[4], ipv4_head[5]))
+                print(self.TAB_1 + 'IPv4 Packet Results:')
+                print(self.TAB_2 + 'Version: {}, Header Length: {}, TTL: {}'.format(ipv4_head[0], ipv4_head[1], ipv4_head[2]))
+                print(self.TAB_2 + 'Protocol: {}, Source: {}, Target: {}\n\n\n'.format(ipv4_head[3], ipv4_head[4], ipv4_head[5]))
+                if ipv4_head[3] == 6:
+                    tcp = self.tcpHead(ipv4_head[6])
+                    print(self.TAB_1 + 'TCP Segment Results:')
+                    print(self.TAB_1 + 'Source Port: {}, Destination Port: {}'.format(tcp[0], tcp[1]))
+                    print(self.TAB_2 + 'Sequence: {}, Acknowledgement: {}'.format(tcp[2], tcp[3]))
+                    print(self.TAB_2 + 'Flags:')
+                    print(self.TAB_3 + 'URG: {}, ACK: {}, PSH: {}'.format(tcp[4], tcp[5], tcp[6]))
+                    print(self.TAB_3 + 'RST: {}, SYN: {}, FIN: {}'.format(tcp[7], tcp[8], tcp[9]))
+                    if len(tcp[10]) > 0:
+                        #HTTP REQUEST
+                        if tcp[0] == 80 or tcp[1] == 80:
+                            print(self.TAB_3+ 'HTTP Data:')
+                        else:
+                            print(self.TAB_2 + 'TCP Data:')
+                            print(self.TAB_3 + tcp[10])
+
 
     def ethernetHead(self, raw_data): #Parsing the Ethernet Frames
         destination, source, prototype = struct.unpack('! 6s 6s H', raw_data[:14])
@@ -42,6 +61,18 @@ class PacketSniffer:
         data = raw_data[header_length:]
         source = self.getIp(source); target = self.getIp(target)
         return version, header_length, timetolive, protocol, source, target, data
+
+    def tcpHead(self, raw_data):
+        source_port, destination_port, sequence, acknowledgement, offset_reserved_flags = struct.unpack('! H H L L H', raw_data[:14])
+        offset = (offset_reserved_flags >> 12) * 4
+        flag_urg = (offset_reserved_flags & 32) >> 5
+        flag_ack = (offset_reserved_flags & 16) >> 4
+        flag_psh = (offset_reserved_flags & 8) >> 3
+        flag_rst = (offset_reserved_flags & 4) >> 2
+        flag_syn = (offset_reserved_flags & 2) >> 1
+        flag_fin = offset_reserved_flags & 1
+        data = raw_data[offset:]
+        return source_port, destination_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data
 
 
 
